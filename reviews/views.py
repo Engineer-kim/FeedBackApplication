615ce2c66ce95af21f5f43c8268de30f718ec79f
Engineer-kim <ddang0103@naver.com>
2024-11-09 19:46:42 +0900

    Feature: ListView 사용하여 간단하게 모든 데이터 조회시 조회 가능함 기존의 Template view 에서의 별도의 함수인 get_context_data를 작성하지 않아도 가능 단, 클라이언트에서 사용할 객체명을 지정해야함 자세한 내용은 코드 속 주석 참조

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,4 @@
+from lib2to3.fixes.fix_input import context
 from urllib import request
 
 from django.http import HttpResponseRedirect
@@ -5,6 +6,8 @@ from django.shortcuts import render
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+from .models import Review
+from django.views.generic import ListView
 
 
 
@@ -57,3 +60,30 @@ class ThankYouView(TemplateView):
 
 # def thank_you(request):
 #     return render(request, "reviews/thank_you.html")
+
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     reviews = Review.objects.all()
+    #     context["reviews"] = reviews
+    #     return context
+
+    model = Review
+    context_object_name = "reviews" # 클라이언트에서 사용할 객체명을 명시, 명시해야 클라이언트에서 사용가능 그게 아니라면 디폴트값인 object_list 를 사용해야함
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs.get("id")
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
+        return context
